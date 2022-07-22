8ea19a962e8e630652d41868382c0064ed70b72e
Salma Hossam elden Hassan Mohamed <88389052+SalmHossam@users.noreply.github.com>
2022-07-22 09:55:21 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -4,27 +4,26 @@ void main() {
   runApp((MaterialApp(home:
   Scaffold(backgroundColor: Colors.white ,
 
-    body: Row(children: [
-      Expanded(child:
-      Container(color: Colors.red,height: 50,width: 50
-        ,)),
-      Expanded(child:
-      Container(color: Colors.yellow,height: 50,width: 50
-        ,)),
-      SizedBox(width: 100,height: 100,),
-      Column(children: [
-        Expanded(child:
-      Container(color: Colors.red,height: 50,width: 50
-        ,)),
-      Expanded(child:
-      Container(color: Colors.red,height: 50,width: 50
-      ,))
-  ],)
-    ]) ,
+    body: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,children: [
+      Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,children: [
 
-  )
+      Container(color: Colors.red,height: 100,width: 100
+        ,),
+
+      Container(color: Colors.yellow,height: 100,width: 100
+        ,),
+      ]) ,
+      Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,children: [
+
+      Container(color: Colors.red,height: 100,width: 100
+        ,),
+
+      Container(color: Colors.red,height: 100,width: 100
+      ,)
+  ],)
+
+
+]))
 
     ,)));
 }
-
-
